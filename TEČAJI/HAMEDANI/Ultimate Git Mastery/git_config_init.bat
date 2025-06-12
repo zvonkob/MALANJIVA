baaae97ce8f248e9938886b9a57335435b7e0198
Zvonko Bregar <anton.bregar@gmail.com>
2025-06-12 11:26:29 +0200

    Hamedani Ultimate Git Mastery tečaj

---
+++ TEČAJI/HAMEDANI/Ultimate Git Mastery/git_config_init.bat
@@ -0,0 +1,7 @@
+git config --global user.name "Zvonko Bregar"
+
+git config --global user.email anton.bregar@gmail.com
+
+git config --global core.editor "code wait"
+
+git config --global core.autocrlf true
